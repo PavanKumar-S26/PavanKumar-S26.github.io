3a546f75f2b8c3c50d49e655fcbab50669b5ae22
PavanKumar-S26 <pavankumarsetti60@gmail.com>
2026-09-12 10:30:15 +0530

    Add portfolio JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => navLinks.classList.remove("open"));
+});
